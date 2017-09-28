eeb43d8ae8d55636383d63fc99b4419e762869e5
Homann Fritz <fhomann@lcs-mba3118.lcs.on.ca>
2017-09-28 11:25:11 -0400

    start to the sun image

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -1,39 +1,20 @@
- /*:
- 
- # playground
- 
- *noun*: a place where people can play
- 
- ##
- 
- Use this playground to experiment with the Canvas class.
- 
- Your goals are to:
- 
- * learn something about order of statements (does order matter?)
- * get familiar with using a Playground
- 
- Have fun!
- 
- */
 // These are some required statements to make this playground work.
 import Cocoa
 import PlaygroundSupport
 
 // Create a new canvas
 let canvas = Canvas(width: 300, height: 500)
-
-// Draw a circle at the origin with radius of 50 pixels
-canvas.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
-
-// Show where the origin is
+ 
+// Draw the axes
 canvas.drawAxes()
 
-// Draw an ellipse in a different color at the centre of the canvas
-canvas.fillColor = Color.green
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 100)
-// add a rectangle to the top of the screen
- canvas.drawRectangle(centreX: 150, centreY: 400, width: 100, height: 50)
+// draw beckground
+canvas.fillColor = Color.blue
+canvas.drawRectangle(bottomLeftX: 50, bottomLeftY: 50, width: 50, height: 50)
+// Draw the sun
+canvas.drawEllipse(centreX: 0, centreY: 490, width: 60, height: 60, borderWidth: 0)
+
+// Draw the rays
 
 // This code is necessary to see the result in the Assistant Editor at right
 PlaygroundPage.current.liveView = canvas.imageView
