7f472f0113bbe6934bdd30670108d791e2dfe57b
Homann Fritz <fhomann@lcs-mba3118.lcs.on.ca>
2017-09-28 17:07:37 -0400

    sun and clouds....

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -10,10 +10,14 @@ canvas.drawAxes()
 
 // draw beckground
 canvas.fillColor = Color.blue
-canvas.drawRectangle(bottomLeftX: 50, bottomLeftY: 50, width: 50, height: 50)
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
 // Draw the sun
-canvas.drawEllipse(centreX: 0, centreY: 490, width: 60, height: 60, borderWidth: 0)
-
+canvas.fillColor = Color.yellow
+canvas.drawEllipse(centreX: 10, centreY: 480, width: 100, height: 100, borderWidth: 0)
+// draw clouds
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 280, centreY: 430, width: 120, height: 120, borderWidth: 0)
+canvas.drawEllipse(centreX: 230, centreY: 480, width: 120, height: 120, borderWidth: 0)
 // Draw the rays
 
 // This code is necessary to see the result in the Assistant Editor at right
